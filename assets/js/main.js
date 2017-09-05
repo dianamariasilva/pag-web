8810b0b31fd9b8d4c052a9797c6e46841afe528b
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-05 02:22:07 -0500

    boton arreglado y validacion de datos con sweet alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,38 @@
 $(document).ready(function() {
     $('#send').click(function(){
         if($("#email").val().indexOf('@') == -1 || $("#email").val().indexOf('.') == -1) {
-            alert('Email inválido');
-            return false;
+            swal({
+              title: 'Datos inválidos',
+              html: $('<div>')
+                .addClass('some-class')
+                .text('Intente de nuevo.'),
+              animation: false,
+              customClass: 'animated tada'
+            })
+        }else if($("#email").val().indexOf('@') == -1 || $("#email").val().indexOf('.') == -1 && $("#phone").val()> 999999999 && $("#phone").val()< 900000000){
+            swal({
+              title: 'Email y teléfono inválidos',
+              html: $('<div>')
+                .addClass('some-class')
+                .text('Intente de nuevo.'),
+              animation: false,
+              customClass: 'animated tada'
+            })
+        }else if($("#phone").val()> 999999999 || $("#phone").val()< 900000000){
+            swal({
+              title: 'Teléfono inválido',
+              html: $('<div>')
+                .addClass('some-class')
+                .text('Intente de nuevo.'),
+              animation: false,
+              customClass: 'animated tada'
+            })
+        }else{
+            swal(
+              'Usuario registrado',
+              'Datos validados!',
+              'success'
+            )
         }
-
-        alert('El correo introducido no es válido');
     });
 });
