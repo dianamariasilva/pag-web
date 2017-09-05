ffb3d02ba24b8e1c2da4a245fea4434b139453e7
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-05 02:51:09 -0500

    titulo parapadeante

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,14 @@ $(document).ready(function() {
         }
     });
 });
+
+
+var estado=true;  
+setTimeout("ver()",900);  
+function ver (){  
+    estado=!estado;  if(estado==true)  texto1.style.visibility="visible";
+      else  texto1.style.visibility="hidden";  
+      setTimeout("ver()",900);  
+}
+
+ver();
